a52722ce7e321cc3ab15a3d58d3cee112f3d197b
pcdro <pcdro_torres@hotmail.com>
2017-07-04 23:19:52 -0300

    Secret word recognition

--- clojure-hangman/src/hangman/core.clj
+++ clojure-hangman/src/hangman/core.clj
@@ -1,18 +1,36 @@
 (ns hangman.core
   (:gen-class))
 
+(use '[clojure.string :only (join)])
+
 (def life-total 6)
 
 (defn you-lose [] (println "You lose :("))
 
-(defn run-game [life-count] 
+(defn you-win [] (println "You win :)"))
+
+(defn print-remaining-life 
+  [current-life] 
+  (println (clojure.string/join ["Remaining Life: " current-life])))
+
+(defn remaining-letters 
+  [secret-word hits]
+  (remove (fn [letter] (contains? hits (str letter))) secret-word))
+
+(defn discovered-word? 
+  [secret-word hits] 
+  (empty? (remaining-letters secret-word hits)))
+
+(defn run-game [life-count secret-word hits] 
   (if (= life-count 0)
     (you-lose)
-    (do
-      (println life-count)
-      (run-game (dec life-count)))))
+    (if (discovered-word? secret-word hits)
+      (you-win)
+      (do
+        (println "Guess a letter!")
+        (print-remaining-life (dec life-count))
+        (run-game (dec life-count) secret-word hits)))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (run-game life-total))
+  (run-game life-total "TEST" #{"A", "B"}))
