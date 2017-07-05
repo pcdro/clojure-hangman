c5320c73c390fe601d646ef416cae66b6b806c47
pcdro <pcdro_torres@hotmail.com>
2017-07-04 21:44:42 -0300

    Initial life logic

--- clojure-hangman/src/hangman/core.clj
+++ clojure-hangman/src/hangman/core.clj
@@ -1,11 +1,18 @@
 (ns hangman.core
   (:gen-class))
 
-(def life-total 5)
+(def life-total 6)
 
 (defn you-lose [] (println "You lose :("))
+
+(defn run-game [life-count] 
+  (if (= life-count 0)
+    (you-lose)
+    (do
+      (println life-count)
+      (run-game (dec life-count)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println life-total))
+  (run-game life-total))
