7d7c9d6c0c8b6bc9c5132e31985d0fb241e2861f
pcdro <pcdro_torres@hotmail.com>
2017-07-05 00:42:29 -0300

    Spelling correction

--- clojure-hangman/project.clj
+++ clojure-hangman/project.clj
@@ -1,5 +1,5 @@
 (defproject hangman "0.1.0-SNAPSHOT"
-  :description "An simples handman game in console (:"
+  :description "A simple handman game in console (:"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
